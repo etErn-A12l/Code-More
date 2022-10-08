7990c3e2b1f8efc1a6cebde0048705190083a536
Sreejan Naru <sreejannaru34@gmail.com>
2022-10-08 21:46:39 +0530

    Routes

--- Code/Website/AngelaYu's Bootcamp/20 Express js/server.js
+++ Code/Website/AngelaYu's Bootcamp/20 Express js/server.js
@@ -2,14 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) => {    // Sends a response to root directory
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/about', (req, res) => {
+    res.send("Nothing about me")    // 127.0.0.1:3000/about
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)  // starts listening on port 3000
 })
 
 // node server.js
 
-// http://localhost:3000
+// localhost:3000
