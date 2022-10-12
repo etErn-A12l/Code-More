d5a826496d5a8e13250eadfc8af27279fb398ca1
Sreejan Naru <sreejannaru34@gmail.com>
2022-10-12 23:08:09 +0530

    Added Comments

--- Code/C/Data Structures Using C/Sort/PDF_Merge_Sort.c
+++ Code/C/Data Structures Using C/Sort/PDF_Merge_Sort.c
@@ -1,8 +1,8 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 #define max 10
 
-int a[10] = {10, 14, 19, 26, 27, 31, 33, 35, 42, 44};
+int a[10] = {27, 31, 10, 35, 42, 44, 14, 19, 26, 33};
 int b[10];
 
 void merging(int low, int mid, int high)
@@ -30,13 +30,24 @@ void merging(int low, int mid, int high)
 void sort(int low, int high)
 {
     int mid;
+
     if (low < high)
     {
         mid = (low + high) / 2;
+
+        // printf("\n\n sort(low, mid);  =   sort(%d, %d);", low, mid);
+        // printf("\n sort(%d, %d);", a[low], a[mid]);
         sort(low, mid);
+
+        // printf("\n\n sort(mid + 1, high); =   sort(%d, %d);", mid + 1, high);
+        // printf("\n sort(%d, %d);", a[mid + 1], a[high]);
         sort(mid + 1, high);
+
+        // printf("\n\n merging(low, mid, high); =   merging(%d, %d, %d);", low, mid, high);
+        // printf("\n merging(%d, %d, %d);", a[low], a[mid], a[high]);
         merging(low, mid, high);
     }
+
     else
     {
         return;
@@ -49,14 +60,14 @@ int main()
 
     printf("List before sorting\n");
 
-    for (i = 0; i <= max; i++)
+    for (i = 0; i < max; i++)
         printf("%d ", a[i]);
 
-    sort(0, max);
+    sort(0, max - 1);
 
     printf("\nList after sorting\n");
 
-    for (i = 0; i <= max; i++)
+    for (i = 0; i < max; i++)
         printf("%d ", a[i]);
 
     return 0;
