81ef2ac5babd1c39c19acedea38c4e0a03280f01
Sreejan Naru <sreejannaru34@gmail.com>
2022-10-14 22:24:41 +0530

    new Problem added

--- Code/Test/C/test.c
+++ Code/Test/C/test.c
@@ -28,20 +28,50 @@
 //     return 0;
 // }
 
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <math.h>
+// #include <stdlib.h>
 
-int main() 
+// int main()
+// {
+
+//     char s[100];
+
+//     scanf("%[^\n]%*c", &s);
+//     printf("Hello, World!  ");
+//     printf("\n%s",s);
+
+//     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
 {
-	
-    char s[100];
-    
-    scanf("%[^\n]%*c", &s);
-    printf("Hello, World!  ");
-    printf("\n%s",s);
-  	
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    int size, element, first_digit, last_digit;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    printf("\nEnter the elements of the array: \n");
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("Enter %dth element: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        element = arr[i];
+        first_digit = element / 10;
+        last_digit = element % 10;
+        if (first_digit - last_digit == 5 || first_digit - last_digit == -5)
+            printf("\nGot difference 5: %d", element);
+    }
     return 0;
 }
