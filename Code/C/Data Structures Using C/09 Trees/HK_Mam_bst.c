0bde0ee54d893bddbed513419df0dc1bfcde40b3
Sreejan Naru <sreejannaru34@gmail.com>
2022-10-14 22:25:15 +0530

    Binary Search Tree

---
+++ Code/C/Data Structures Using C/09 Trees/HK_Mam_bst.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct bstNode
+{
+    int data;
+    struct bstNode *left, *right;
+};
+
+struct bstNode *root = NULL, *temp = NULL;
+
+struct bstNode *insert();
+struct bstNode *allocate();
+void insert_inside(struct bstNode *root);
+void inorder(struct bstNode *);
+void preorder(struct bstNode *);
+void postorder(struct bstNode *);
+
+int main()
+{
+    int option = 0;
+    do
+    {
+        printf("\n\n**** MENU *****");
+        printf("\n1. Insert Node");
+        printf("\n2. Inorder Display");
+        printf("\n3. Preorder Display");
+        printf("\n4. Postorder Display");
+        printf("\n\nEnter your choice: ");
+        scanf("%d", &option);
+		system("cls");
+        switch (option)
+        {
+        case 1:
+            root = insert();
+            break;
+        case 2:
+            if (root == NULL)
+                printf("\nTree is EMPTY !");
+            else
+                inorder(root);
+            break;
+        case 3:
+            if (root == NULL)
+                printf("\nTree is EMPTY !");
+            else
+                preorder(root);
+            break;
+        case 4:
+            if (root == NULL)
+                printf("\nTree is EMPTY !");
+            else
+                postorder(root);
+            break;
+        default:
+            printf("\nWRONG Choice !");
+            break;
+        }
+    } while (option < 5);
+
+    return 0;
+}
+
+struct bstNode *insert()
+{
+    temp = allocate();
+    if (root == NULL)
+    {
+        root = temp;
+        return root;
+    }
+    else
+    {
+        insert_inside(root);
+        return root;
+    }
+}
+
+struct bstNode *allocate()
+{
+    temp = (struct bstNode *)malloc(sizeof(struct bstNode));
+    printf("\nEnter the data for the new element: ");
+    scanf("%d", &temp->data);
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+void insert_inside(struct bstNode *root)
+{
+    if (temp->data < root->data && root->left == NULL)
+        root->left = temp;
+    else if (temp->data < root->data && root->left != NULL)
+        insert_inside(root->left);
+    else if (temp->data > root->data && root->right == NULL)
+        root->right = temp;
+    else if (temp->data > root->data && root->right != NULL)
+        insert_inside(root->right);
+}
+
+void inorder(struct bstNode *root)
+{
+    if (root != NULL)
+    {
+        inorder(root->left);
+        printf("\t%d", root->data);
+        inorder(root->right);
+    }
+}
+
+void preorder(struct bstNode *root)
+{
+    if (root != NULL)
+    {
+        printf("\t%d", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(struct bstNode *root)
+{
+    if (root != NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        printf("\t%d", root->data);
+    }
+}
