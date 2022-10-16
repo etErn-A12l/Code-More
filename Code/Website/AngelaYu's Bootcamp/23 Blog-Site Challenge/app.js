a35b3d1dbc7adef9aff258c4e1bae40fb8d7737c
Sreejan Naru <sreejannaru34@gmail.com>
2022-10-16 07:55:11 +0530

    Challenge 21 Done 😎

--- Code/Website/AngelaYu's Bootcamp/23 Blog-Site Challenge/app.js
+++ Code/Website/AngelaYu's Bootcamp/23 Blog-Site Challenge/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -10,7 +11,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-var new_posts = [];   // array for new posts
+var new_posts = []; // array for new posts objects
 
 app.set('view engine', 'ejs');
 
@@ -23,7 +24,8 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("home", {
-    paragraph1: homeStartingContent
+    paragraph1: homeStartingContent,
+    posts: new_posts
   });
 });
 
@@ -43,6 +45,20 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
+app.get('/posts/:post_name', (req, res) => {
+
+  new_posts.forEach(Element => {
+
+    if (_.lowerCase(Element.title) === _.lowerCase(req.params.post_name)) {
+      res.render("post", {
+        Title: Element.title,
+        Content: Element.content
+      })
+    }
+  });
+
+});
+
 app.post('/compose', (req, res) => {
 
   var new_post = {
@@ -51,8 +67,7 @@ app.post('/compose', (req, res) => {
   };
 
   new_posts.push(new_post);
-
-  res.send(new_posts)
+  res.redirect('/');
 });
 
 
