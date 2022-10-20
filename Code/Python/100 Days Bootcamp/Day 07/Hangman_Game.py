1fad9ebe90569e660be0e79e35f862e1c4cbdc47
Sreejan Naru <sreejannaru34@gmail.com>
2022-10-21 00:57:45 +0530

    Challenge 1 Done

--- Code/Python/100 Days Bootcamp/Day 07/Hangman_Game.py
+++ Code/Python/100 Days Bootcamp/Day 07/Hangman_Game.py
@@ -6,21 +6,16 @@ word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
+chosen_word = word_list[random.randint(0, len(word_list)-1)] 
+
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+guess = input("Guess a letter: ").lower()  
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
-chosen_word = word_list[random.randint(0, len(word_list)-1)]    # Randomly choose any word from the list
-
-print(chosen_word)
-
-guess = input("Guess a letter: ")   # Take input from the user
-
-guess = guess.lower()   # convert to lowercase
-
-print(guess)
-
-if chosen_word.count(guess) > 0:        # Check if the letter is present in the chosen word
-    print("Present !")
-else:
-    print("Absent !")
+for letter in chosen_word:
+    if guess == letter:
+        print("Match !")
+    else:
+        print("NO Match !")
