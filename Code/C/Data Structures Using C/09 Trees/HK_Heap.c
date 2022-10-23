a9cff47d4cf3994b783a10c88d61ca9de24bf058
Sreejan Naru <sreejannaru34@gmail.com>
2022-10-23 09:14:57 +0530

    Heap Sort

---
+++ Code/C/Data Structures Using C/09 Trees/HK_Heap.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 10
+
+int heap[MAX] = {12, 56, 23, 85, 35, 90, 52, 47, 72, 84};
+
+void insert(int heap[]);
+void heapify(int heap[], int size, int i);
+void heapSort(int heap[], int size);
+void swap(int *a, int *b);
+void display(int heap[]);
+
+int main(int argc, char const *argv[])
+{
+    int option;
+    system("cls");
+    do
+    {
+        printf("\n\n**** MENU ****");
+        // printf("\n1. Insert");
+        printf("\n2. Display");
+        printf("\n3. Heap Sort");
+        printf("\n\nEnter your choice: ");
+        scanf("%d", &option);
+        system("cls");
+
+        // if (option == 1)
+        //     insert(heap);
+        if (option == 2)
+            display(heap);
+        else if (option == 3)
+            heapSort(heap, MAX);
+        else
+            printf("\nWrong Choice !");
+    } while (option < 4);
+
+    return 0;
+}
+
+void insert(int heap[])
+{
+    printf("\nEnter elements for the heap: \n");
+    for (int i = 0; i < MAX; i++)
+    {
+        printf("Element for %dth element: ", i);
+        scanf("%d", &heap[i]);
+    }
+}
+
+void heapify(int heap[], int size, int i)
+{
+    int index = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (heap[index] < heap[left] && left < size)
+        index = left;
+    if (heap[index] < heap[right] && right < size)
+        index = right;
+
+    if (i != index)
+    {
+        swap(&heap[i], &heap[index]);
+        heapify(heap, size, index);
+    }
+}
+
+void heapSort(int heap[], int size)
+{
+    for (int i = size / 2 - 1; i >= 0; i--)
+    {
+        heapify(heap, MAX, i);
+    }
+
+    for (int i = size - 1; i >= 0; i--)
+    {
+        swap(&heap[0], &heap[i]);
+        heapify(heap, i, 0);
+    }
+    printf("\nSorted the Heap !");
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void display(int heap[])
+{
+    printf("\nBelow is the heap: \n\n");
+    for (int i = 0; i < MAX; i++)
+    {
+        printf("  %d", heap[i]);
+    }
+}
