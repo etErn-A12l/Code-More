76a8fe085142ba353c9d58c2e9b85b1ce98af8df
Sreejan Naru <sreejannaru34@gmail.com>
2022-11-13 19:42:55 +0530

    Class

--- Code/JAVA/Udemy/user_input.java
+++ Code/JAVA/Udemy/user_input.java
@@ -6,12 +6,12 @@ public class user_input {
         Scanner input = new Scanner(System.in);
 
         // Takes an input of a string
-        System.out.println("Enter a text baby: ");
+        System.out.print("Enter a text baby: ");
         String text = input.nextLine();
         System.out.println("Here is your text " + text);
 
         // Takes an input of a intezer
-        System.out.println("Enter a number baby: ");
+        System.out.print("Enter a number baby: ");
         int a = input.nextInt();
         System.out.println("Here is your intezer : " + a);
 
