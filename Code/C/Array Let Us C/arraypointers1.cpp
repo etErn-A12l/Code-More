48f2b9b1107f692d5c2b0c5fa56700b677907987
Sreejan Naru <sreejannaru34@gmail.com>
2022-11-13 09:50:14 +0530

    format

--- Code/C/Array Let Us C/arraypointers1.cpp
+++ Code/C/Array Let Us C/arraypointers1.cpp
@@ -1,17 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-    int num[]={24,34,12,44,56,17};
-    int i,*j;
-    int as=89;
+    int num[] = {24, 34, 12, 44, 56, 17};
+    int i, *j;
+    int as = 89;
     float er;
     er = as;
-    printf("%f",er);
-    j=&num[0];//assign address of zeroth element
-    for (i=0;i<=5;i++)
+    printf("%f", er);
+    j = &num[0]; // assign address of zeroth element
+    for (i = 0; i <= 5; i++)
     {
-        printf("address =%u element =%d\n",j,*j);
-        j++;//increment pointer to point to next location 
+        printf("address =%u element =%d\n", j, *j);
+        j++; // increment pointer to point to next location
     }
     return 0;
 }
