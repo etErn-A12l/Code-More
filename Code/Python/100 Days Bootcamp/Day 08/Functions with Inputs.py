a4bdde089f9598e742a134c8102858e81accb3ae
Sreejan Naru <sreejannaru34@gmail.com>
2022-12-06 02:25:32 +0530

    Day 8 Done

--- Code/Python/100 Days Bootcamp/Day 08/Functions with Inputs.py
+++ Code/Python/100 Days Bootcamp/Day 08/Functions with Inputs.py
@@ -1,11 +1,22 @@
+
+# Function with no arguments
 def greet():
     print('Good Morning')
     print('Make this day count')
 
 
-
+# Function with arguments
 def greet_with_name(name):
     print(f"Good night {name}")
     print(f"{name} you are awesome")
 
+
+def greet_with(name, size):
+    print(f"Hello there, I am {name} and my size is {size}")
+
+
 greet_with_name("xron")
+# Calling Function with positional arguments
+greet_with("xron", "75 kgs")
+# Calling Function with keyword arguments
+greet_with(size="75 kgs", name="xron")   # Order doesn't matter
---
+++ Code/Test/python/if_else.py
@@ -0,0 +1,11 @@
+app = 60
+
+
+if app == 5:
+    print('Printed')
+elif app < 10:
+    print("Again print !")
+elif app > 2:
+    print("Again again print !")
+else:
+    print("Else also printed ! wtf ?")
