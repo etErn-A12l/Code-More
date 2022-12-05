f83d251d8af18c2048bbb43c7290d1bc8dfb27c0
Sreejan Naru <sreejannaru34@gmail.com>
2022-12-05 17:35:38 +0530

    continue

---
+++ Code/Python/100 Days Bootcamp/Day 08/Functions with Inputs.py
@@ -0,0 +1,11 @@
+def greet():
+    print('Good Morning')
+    print('Make this day count')
+
+
+
+def greet_with_name(name):
+    print(f"Good night {name}")
+    print(f"{name} you are awesome")
+
+greet_with_name("xron")
