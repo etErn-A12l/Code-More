acb23420d5350fb549c1b2806bfff1d59cfc8cc3
Xron Trix <sreejannaru34@gmail.com>
2023-02-05 12:50:23 +0530

    format

--- Code/Test/python/Piyali_Patter.py
+++ Code/Test/python/Piyali_Patter.py
@@ -1,8 +1,8 @@
-n=5
+n = 5
 
 for i in range(n):
-    for k in range(n-i-1):
-        print("  ",end='')
-    for j in range(2*i+1):
-        print("* ",end='')
+    for k in range(n - i - 1):
+        print("  ", end="")
+    for j in range(2 * i + 1):
+        print("* ", end="")
     print()
