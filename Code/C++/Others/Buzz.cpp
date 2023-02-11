ca2ba41c6e0adcf98a9c4bb7ad3c5aa2affcce2a
Xron Trix <sreejannaru34@gmail.com>
2023-02-11 23:27:00 +0530

    optimised

--- Code/C++/Others/Buzz.cpp
+++ Code/C++/Others/Buzz.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
   int n;
   cout << "Please enter a number: ";
   cin >> n;
-  if (n % 7 == 0 || n % 10 == 7)
+  
+  if (n % 70 == 0)     // Check if divisible by both 7 and 10
     cout << n << " is a buzz number " << endl;
   else
     cout << n << " is not a buzz number " << endl;
+    
   return 0;
 }
