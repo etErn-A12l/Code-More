ecfa1db4879964e724f1ee9f25f22e85a30ad68d
Xron Trix <sreejannaru34@gmail.com>
2023-02-24 02:08:09 +0530

    iteration

--- Code/C/Others/HK mam TSP Prob 2/myBatSol.c
+++ Code/C/Others/HK mam TSP Prob 2/myBatSol.c
@@ -20,18 +20,23 @@ short bestFitness(struct bat x[]);
 void adjustFreq(struct bat x[]);
 void updateVelocity(struct bat x[], short Index);
 void updatePosition(struct bat x[]);
+void genLocalSolutions(struct bat x[], struct bat y[]);
+void updateLoudPulse(struct bat x[]);
 
 int main(int argc, char const *argv[])
 {
     srand((unsigned int)time(NULL)); // seedind with the value of current time
 
-    float R = (float)rand() / (float)(RAND_MAX);
+    // Generate random frequencies and loudness
+    float A = (float)rand() / (float)RAND_MAX;
+    float r = (float)rand() / (float)RAND_MAX;
+
     short Iteration = 0;
 
     Initiate(x);
     calFitness(x);
 
-    short bestIndex = bestFitness(x);
+    short bestIndex_X = bestFitness(x);
 
     // for (short i = 0; i < POP_NO; i++)
     //     printf("Fitness = %lf\n", x[i].fitness);
@@ -41,10 +46,36 @@ int main(int argc, char const *argv[])
     while (Iteration++ < ITERATIONS)
     {
         adjustFreq(x);
-        updateVelocity(x, bestIndex);
+        updateVelocity(x, bestIndex_X);
         updatePosition(x);
         calFitness(x);
-        bestIndex = bestFitness(x);
+
+        bestIndex_X = bestFitness(x);
+        float X_bestFitness = x[bestIndex_X].fitness;
+        float R = (float)rand() / (float)(RAND_MAX);
+        struct bat y[POP_NO];
+
+        if (R > r)
+            // Generating new local solutions
+            genLocalSolutions(x, y);
+
+        // Calculating fitness of the new local solutions
+        calFitness(y);
+        short bestIndex_Y = bestFitness(y); // Index of Best fitness value
+
+        if (A < x[bestIndex_X].loudness && y[bestIndex_Y].fitness < X_bestFitness)
+        {
+            // Accepting new solutions
+            for (short i = 0; i < POP_NO; i++)
+            {
+                for (short j = 0; j < DIM; j++)
+                    x[i].position[j] = y[i].position[j];
+                x[i].fitness = y[i].fitness;
+            }
+
+            // reduce loudness and increase puse rate
+            updateLoudPulse(x);
+        }
     }
 
     return 0;
@@ -108,4 +139,23 @@ void updatePosition(struct bat x[])
     for (short i = 0; i < POP_NO; i++)
         for (short j = 0; j < DIM; j++)
             x[i].position[j] = x[i].position[j] + x[i].velocity[j];
+}
+
+void genLocalSolutions(struct bat x[], struct bat y[])
+{
+    float avg_loudness, sum = 0;
+    for (short i = 0; i < POP_NO; i++)
+        sum += x[i].loudness;
+
+    avg_loudness = sum / POP_NO;
+
+    float Eta = ((float)rand() / (float)(RAND_MAX)) * (1 - (-1)) + (-1);
+
+    for (short i = 0; i < POP_NO; i++)
+        for (short j = 0; j < DIM; j++)
+            y[i].position[j] = x[i].position[j] + Eta * avg_loudness;
+}
+
+void updateLoudPulse(struct bat x[])
+{
 }
