e02931514a3bcd43100815c9e155208bd63304c9
Xron Trix <sreejannaru34@gmail.com>
2023-03-22 00:49:27 +0530

    ChatGPT Optimization

--- Code/Python/IOT/IP_Info.py
+++ Code/Python/IOT/IP_Info.py
@@ -1,15 +1,28 @@
-IP_Classes = ["A", "B", "C"]
-Net_Masks = ["255.0.0.0", "255.255.0.0", "255.255.255.0"]
-range_SE = [".0.0.1", ".0.1", ".1", ".254", ".0.254", ".0.0.254"]
+# Define IP Class related information in a dictionary
+ip_info = {
+    "A": {
+        "mask": "255.0.0.0",
+        "range": [".0.0.1", ".0.0.254"]
+    },
+    "B": {
+        "mask": "255.255.0.0",
+        "range": [".0.1", ".0.254"]
+    },
+    "C": {
+        "mask": "255.255.255.0",
+        "range": [".1", ".254"]
+    }
+}
+
+IP_classes = ["A", "B", "C"]
 
 IP = input("\nEnter the IP address: ")
-# IP = "221.255.255.255"
 
 # Splits the octets
 octets = IP.split(".")
 
 for octet in octets:
-    if int(octet) > 255:
+    if not int(octet) in range(256):
         print("\nINVALID IP Address !!\n")
         exit(1)
 
@@ -17,32 +30,27 @@ for octet in octets:
 binctet = "{0:b}".format(int(octets[0])).zfill(8)
 
 IP_class = ""
-Net_Mask = ""
-NetIDoctet = 0
 
 if binctet[0] == "0":
-    IP_class = IP_Classes[0]
-    Net_Mask = Net_Masks[0]
-    NetIDoctet = 1
+    IP_class = IP_classes[0]
 elif binctet[0] == "1" and binctet[1] == "0":
-    IP_class = IP_Classes[1]
-    Net_Mask = Net_Masks[1]
-    NetIDoctet = 2
+    IP_class = IP_classes[1]
 elif binctet[0] == "1" and binctet[1] == "1" and binctet[2] == "0":
-    IP_class = IP_Classes[2]
-    Net_Mask = Net_Masks[2]
-    NetIDoctet = 3
+    IP_class = IP_classes[2]
+
+Net_Mask = ip_info[IP_class]["mask"]
+NetIDoctet = IP_classes.index(IP_class) + 1
 
 NetID = ".".join(octets[:NetIDoctet])
 HostID = ".".join(octets[NetIDoctet:])
-Ren_S = NetID + range_SE[NetIDoctet - 1]
-Ren_E = NetID + range_SE[len(range_SE) - NetIDoctet]
+Ren_S = NetID + ip_info[IP_class]["range"][0]
+Ren_E = NetID + ip_info[IP_class]["range"][-1]
 
 print()
-print("{:<24} {}".format("Given IP Address: ", IP))
-print("{:<24} Class {}".format("Class of Given IP: ", IP_class))
-print("{:<24} {}".format("Net Mask of Given IP: ", Net_Mask))
-print("{:<24} {}".format("Network ID of Given IP: ", NetID))
-print("{:<24} {}".format("Host ID of Given IP: ", HostID))
-print("{:<24} {} - {}".format("Range of IP Network: ", Ren_S, Ren_E))
+print(f"{'Given IP Address:':<24} {IP}")
+print(f"{'Class of Given IP:':<24} Class {IP_class}")
+print(f"{'Net Mask of Given IP:':<24} {Net_Mask}")
+print(f"{'Network ID of Given IP:':<24} {NetID}")
+print(f"{'Host ID of Given IP:':<24} {HostID}")
+print(f"{'Range of IP Network:':<24} {Ren_S} - {Ren_E}")
 print()
