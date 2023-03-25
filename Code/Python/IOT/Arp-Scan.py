53e8c40272088a92c198476037b44a58efbe0124
Xron Trix <sreejannaru34@gmail.com>
2023-03-26 01:55:17 +0530

    done

--- Code/Python/IOT/Arp-Scan.py
+++ Code/Python/IOT/Arp-Scan.py
@@ -8,7 +8,7 @@ def random_ip():
 
 # Generate a random MAC address
 def random_mac():
-    mac = [0x00, 0x16, 0x3e,
+    mac = [0xa7, 0x16, 0x3e,
            random.randint(0x00, 0x7f),
            random.randint(0x00, 0xff),
            random.randint(0x00, 0xff)]
@@ -26,8 +26,35 @@ while count < 30:
         count += 1
 
 
-my_ip = "192.168." + ".".join(str(random.randint(0, 255)) for _ in range(2))
+my_ip = random.choice(list(dictionary.keys()))
+my_mac = dictionary[my_ip]
 
-for ip in dictionary:
-    print(f"{ip :<15} : {dictionary[ip]}")
+print(dictionary)
+print()
+choice = input("ARP or R-ARP ? (1/2): ").lower()
+
+if choice == "y":
+
+    IP = input("Enter The IP Address to Find: ")
+
+    print(f"\nSender:\n{'IP' :<4} {my_ip}\n{'MAC' :<4} {my_mac}")
+    print(f"\nTarget:\n{'IP' :<4} {IP}\n{'MAC' :<4} " + ":".join("00" for _ in range(6)))
+    if IP not in dictionary:
+        print("\nReciever not FOUND !")
+    else:
+        print(f"\nReciever:\n{'IP' :<4} {IP}\n{'MAC' :<4} {dictionary[IP]}")
+
+else:
+
+    inverted_dict = {value: key for key, value in dictionary.items()}
+    MAC = input("Enter The MAC Address to Find: ")
+
+    print(f"\nSender:\n{'IP' :<4} {my_ip}\n{'MAC' :<4} {my_mac}")
+    print(f"\nTarget:\n{'IP' :<4} " + ".".join("0" for _ in range(4)) + f"\n{'MAC' :<4} {MAC}")
+    if MAC not in inverted_dict:
+        print("\nReciever not FOUND !")
+    else:
+        print(f"\nReciever:\n{'IP' :<4} {inverted_dict[MAC]}\n{'MAC' :<4} {MAC}")
+
+
 
