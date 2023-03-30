243396e6e35701e85187c31a6b807369747f498e
Xron Trix <sreejannaru34@gmail.com>
2023-03-31 02:06:42 +0530

    fixing bug

--- Code/Python/IOT/CRC.py
+++ Code/Python/IOT/CRC.py
@@ -20,16 +20,45 @@ dvdent = o_data + "0" * crc_len
 print(f"{dvdent} / {dvsr}")
 
 inDiv = dvdent[: len(dvsr)]
+
 r = ""
-for i in range(len(dvdent) - len(dvsr)):
+i = len(dvsr) - 1
+print(f"Initilly i = {i}")
+while i <= len(dvdent):  # Until we reach the last bit of dvdent
 
-    r = xor(inDiv, chck(inDiv, dvsr))
-    print(f"XOR ({inDiv} , {chck(inDiv,dvsr)})")
-    r += dvdent[i + len(dvsr)]
-    print(f"{r[:-1]}" + f" + {r[-1]}")
-    inDiv = r[1:]
+    r = xor(inDiv, chck(inDiv, dvsr))  # bit wise xor operation
+    print(f"XOR ({inDiv} , {chck(inDiv,dvsr)}) = ({r})")
 
-print(r[2:])
+    if i == len(dvdent) - 1:  # If last bit is already used, break
+        break
+
+    c = 0
+    for k in range(len(r)):  # Checking how many bits are '0' from left to right
+        if r[k] != "1":
+            c += 1
+        else:
+            break
+
+    r = r[c:]  # Discarding '0' bits
+
+    for k in range(c + 1):
+        r += dvdent[k + i]
+
+    inDiv = r
+    i += c
+
+    print(f"Updated r = {r}, i = {i}")
+
+
+# for i in range(len(dvdent) - len(dvsr)):
+
+#     r = xor(inDiv, chck(inDiv, dvsr))
+#     print(f"XOR ({inDiv} , {chck(inDiv,dvsr)})")
+#     r += dvdent[i + len(dvsr)]
+#     print(f"{r[:-1]}" + f" + {r[-1]}")
+#     inDiv = r[1:]
+
+print(f"Final r = {r[2:]}")
 
 transmitter = o_data + r[2:]
 
